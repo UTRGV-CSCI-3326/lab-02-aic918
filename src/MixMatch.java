266509a17fbb7dbfc779839d212955d28709fc09
Alexis Cano <156945503+aic918@users.noreply.github.com>
2025-01-31 23:07:11 +0000

    Commit 1

---
+++ src/MixMatch.java
@@ -0,0 +1,26 @@
+
+// Mix Match
+
+public class MixMatch {
+    public static void main (String [] args) {
+
+String var1 = "String: Data type that allows for a string of characters to represent a text.";
+        System.out.println(var1);
+char var2 = 'A';
+        System.out.println("char: Data type that stores a single character --> " + var2);
+short var3 = 113;
+        System.out.println("short: Data type that can store values from -32,768 to 32,767 --> " + var3);
+int var4 = 123;
+        System.out.println("int: Data type that stores an integer --> " + var4);
+long var5 = 1000000000L;
+        System.out.println("long: Data type that can store integers outside the range of int --> " + var5);
+float var6 = 3.5f; 
+        System.out.println("float: Data type that can store a fractional value --> " + var6); 
+double var7 = 4.23;
+        System.out.println("double: Data type that can store a fractional value but is more precise and can store more values than a float. --> " + var7);
+boolean var8 = true;
+        System.out.println("boolean: Data type that can hold a true or false value --> " + var8);
+
+        
+    }
+}
